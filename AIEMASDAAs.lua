f5f8b2b326935a380754d19bbf2f8a5d8bb810fc
venoXsz <92759797+venoXsz@users.noreply.github.com>
2021-10-19 15:02:32 -0300

    Update and rename AIMASDAAs.lua to AIEMASDAAs.lua

--- AIEMASDAAs.lua
+++ AIEMASDAAs.lua
@@ -1,6 +1,6 @@
 local ui_options = {
-	main_color = Color3.fromRGB(10, 10, 10),
-	min_size = Vector2.new(300, 400),
+	main_color = Color3.fromRGB(41, 74, 122),
+	min_size = Vector2.new(400, 300),
 	toggle_key = Enum.KeyCode.RightShift,
 	can_resize = true,
 }
